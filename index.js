3c72818d078839beef42a10b2da86d152ee15fd0
Kinton_Hiryuu <60929021+KintonHiryuu@users.noreply.github.com>
2025-05-17 21:59:51 +0200

    crash fix
    yeah i'm quite too lazy to implement a correct fix right now, will do later (just kidding, everything is gonna be wiped out and remade correctly)

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { RequestError, VRChatAPI, VRCWebSocket, EventType } = require('vrc-ts');
 const _DISCORD = require("discord.js")
 
-const DiscordWebhook = _DISCORD.WebhookClient = new _DISCORD.WebhookClient({url:"https://discord.com/api/webhooks/1372916995741454357/kHdvHhGCE4L3dAAExavnwPqe-i3EpX34-T6DifKcx_KPWnkNhQ3r9oK5J6HVdIRhau4L"})
+console.log("start")
+const DiscordWebhook = _DISCORD.WebhookClient = new _DISCORD.WebhookClient({ url: "https://discord.com/api/webhooks/1033328149519405126/Vv5dg33YxrSSMG2SgnFu7rXsf9DsoVhue0nMQZMwWNukp8fGv5LAFO2n7x3mwpju5j8Z" })
 
 const api = new VRChatAPI({});
 main();
+
 
 // Instantiate WebSocket
 
@@ -12,8 +14,8 @@ async function main() {
     try {
         await api.login();
         // console.log(`Logged in successfully as ${api.currentUser.displayName}!`);
-        DiscordWebhook.send({content:`Bot connecte`})
-        
+        DiscordWebhook.send({ content: `Bot connecte` })
+
     } catch (error) {
         if (error instanceof RequestError) {
             console.error(`Failed to login: ${error.message}`);
@@ -23,38 +25,51 @@ async function main() {
     }
     let users = []
     for await (user of api.currentUser.friends) {
-        users.push(await (await (api.userApi.getUserById({userId:user}))).displayName)    
+        users.push(await (await (api.userApi.getUserById({ userId: user }))).displayName)
     }
     // console.log(`Tracked : \n - ${users.join("\n - ")}`)
-    DiscordWebhook.send({content:`Amis du compte : \n \`\`\` - ${users.join("\n - ")}\`\`\``})
-    
+    DiscordWebhook.send({ content: `Amis du compte : \n \`\`\` - ${users.join("\n - ")}\`\`\`` })
+
     const ws = new VRCWebSocket({
         vrchatAPI: api,
         eventsToListenTo: [EventType.Friend_Location, EventType.Friend_Offline, EventType.Friend_Online],
     });
-    
+
     ws.on(EventType.All, (data) => {
         console.log(`------------------------------------------------`)
         console.log(data)
         console.log(`------------------------------------------------`)
     })
-    
-    
+
+
     ws.on(EventType.Friend_Online, (data) => {
         console.log(`------------------------------------------------`)
         console.log(data)
-        DiscordWebhook.send({content:`${data.user.displayName} est connecte dans ${data.location}.`})
+        try {
+            DiscordWebhook.send({ content: `${data.user.displayName} est connecte dans ${data.location}.` })
+        } catch (error) {
+
+        }
+
     })
-    
+
     ws.on(EventType.Friend_Offline, (data) => {
         console.log(`------------------------------------------------`)
         console.log(data)
-        DiscordWebhook.send({content:`${data.user.displayName} est deconnecte.`})
+        try {
+            DiscordWebhook.send({ content: `${data.user.displayName} est deconnecte.` })
+        } catch (error) {
+
+        }
     })
-    
+
     ws.on(EventType.Friend_Location, (data) => {
         console.log(`------------------------------------------------`)
         console.log(data)
-        DiscordWebhook.send({content:`${data.user.displayName} est maintenant dans le monde ${data.world?.name || "prive"}`})
+        try {
+            DiscordWebhook.send({ content: `${data.user.displayName} est maintenant dans le monde ${data.world?.name || "prive"}` })
+        } catch (error) {
+
+        }
     })
 }
