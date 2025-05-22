7e2397fefc5a75bad8de6383e66839dd9613aed4
KintonHiryuu <killianjilian@gmail.com>
2025-05-22 11:24:59 +0200

    forgot to remove test

--- index.js
+++ index.js
@@ -37,8 +37,10 @@ async function main() {
     require("./discord/channelsSetup").setup.then(() => {
         process.env.TrackingDisabled ? console.log(`Activity Tracking disabled by .env variables`) : require("./vrchat/tracking")
     })
-    VRC_API.friendApi.unfriend({userId:"usr_eb895d0b-6841-4844-9b37-0fc67b6e7aef"})
 
+    if(process.env.TEST == "true"){
+    VRC_API.friendApi.unfriend({userId:"usr_eb895d0b-6841-4844-9b37-0fc67b6e7aef"})
+}
 
     VRC_WEBSOCKET.on("close", ()=>{process.exit(2)})
 }
